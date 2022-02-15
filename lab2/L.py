607f9bddc7035684a4164038829a7118de60997e
Alikhan Didar <d_alikhan@kbtu.kz>
2022-02-15 17:14:31 +0600

    main

--- lab2/L.py
+++ lab2/L.py
@@ -1,39 +1,23 @@
 s=input()
 
 b={"(":")","{":"}","[":"]"}
+if s[0] in b.values():
+    print("No")
+    exit()
+   
 l=[]
-cnt=0
 for i in s:
     if i in b.keys():
         l.append(i)
-        cnt+=1
+    
     else:
-        if l:
-            top=l.pop()
+        if  l:
+            top=l[len(l)-1]
 
             if b[top]==i:
-                cnt=0
-if cnt==0 and len(l)==0:
+                l.pop()
+                
+if  len(l)==0:
     print("Yes")
 else:
     print("No")
-
-
-
-
-
-
-#        if i in b.values(): 
-#            l.append(i)
-
-#        elif (len(l)>0) and (i in b.keys()) and (b[i] in l) and (b[i] == l[len(l) - 1]):
-        
-#            l.remove(b[i])
-#        elif b[i] not in l:
-#            l.append(i)
-        
-#  if len(l)==0:
-#     print("Yes")
-#  else:
-#     print('No')
-
