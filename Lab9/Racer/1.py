f69964dd31d97afa31857a614fb1ccd62c0f4be6
Alikhan Didar <d_alikhan@kbtu.kz>
2022-04-16 01:49:37 +0600

    main

--- Lab9/Racer/1.py
+++ Lab9/Racer/1.py
@@ -1,5 +1,4 @@
-from pickle import FRAME
-import pygame , os, sys
+import pygame , os, sys,time
 from random import randint as rn
 import random
 
@@ -17,9 +16,9 @@ BLACK = (0, 0, 0)
 WHITE = (255,255,255)
 SCREEN_WIDTH = 600
 SCREEN_HEIGT = 600
-SPEED1 = 1
-SPEED2 = 2
 SCORE = 0
+
+
 
 #main screen
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
@@ -27,66 +26,65 @@ pygame.display.set_caption('RACER')
 image_backround = pygame.image.load(r'C:\pp2_21B030767\Lab8\Circuit.png')
 image_backround = pygame.transform.scale(image_backround,(SCREEN_WIDTH,SCREEN_HEIGHT))
 
+surface_coin = pygame.Surface((60,30))
+surface_coin.fill((255,255,255))
+
+game_start = pygame.image.load('Lab9\Racer\game_start.jpg')
+game_start = pygame.transform.scale(game_start,(SCREEN_WIDTH,SCREEN_HEIGHT))
+screen.blit(game_start,(0,0))
+pygame.display.update()
+
+game_over = pygame.image.load('Lab9\Racer\game_over.png')
+game_over = pygame.transform.scale(game_over,(SCREEN_WIDTH,SCREEN_HEIGHT))
+
+time.sleep(3)
+pygame.mixer.music.load('C:/Pygame/Sountracks/background.wav')
+pygame.mixer.music.play(-1)
 
 
 class Enemy(pygame.sprite.Sprite):
-    def __init__(self):
-        s1 = list(range(1, 5))
-        s2 = list(range(5, 10))
 
-        i = random.choice(s1)
-        j = random.choice(s2)
-
-        self.enemy = os.listdir(r'C:\Pygame\cars')
-       
-        self.image1 = pygame.image.load(os.path.join(f'C:/Pygame/cars/car_{i}.png'))
+    def __init__(self,posx,im,speed):
+        super().__init__()
+        self.speed = speed
+        self.take = False
+        self.image1 = pygame.image.load(os.path.join(f'C:/Pygame/cars/car_{im}.png'))
         self.image1 = pygame.transform.scale(self.image1,(60,116))
 
-        # self.image2 = pygame.image.load(os.path.join(f'C:/Pygame/cars/car_{j}.png'))
-        # self.image2 = pygame.transform.scale(self.image2,(60,116))
 
-        self.rect1 = self.image1.get_rect()
-        self.rect1.center = (rn(150, SCREEN_WIDTH - 325), 0)
-
-        # self.rect2 = self.image2.get_rect()
-        # self.rect2.center = (rn(320, SCREEN_WIDTH - 150), 0)
-
+        self.rect = self.image1.get_rect()
+        self.rect.center = (posx, 0)
         
-    def move(self):
+    def move(self, chainge_pos):
+        if self.take == True:
+            self.speed += 0.125
+        self.take = False
         
-        self.rect1.move_ip(0,SPEED1)
-        # self.rect2.move_ip(0,SPEED2)
-         
-        s1 = list(range(1, 5))
-        s2 = list(range(5, 10))
-
+        self.rect.move_ip(0,self.speed)
+        
+        s1 = list(range(1, 10))
         i = random.choice(s1)
-        j = random.choice(s2)
-
-        if self.rect1.bottom > 600:
-                self.rect1.top = 0
-                self.rect1.center = (rn( 150, 275), 0)
+      
+        
+        if self.rect.bottom > 600:
+                s1 = list(range(1, 10))
+                i = random.choice(s1)
+                self.rect.top = 0
+                self.rect.center = (chainge_pos)
                 self.image1 = pygame.image.load(os.path.join(f'C:/Pygame/cars/car_{i}.png'))
                 self.image1 = pygame.transform.scale(self.image1,(60,116))
         
-        # if self.rect2.bottom > 600 :
-        #         self.rect2.top = 0
-        #         self.rect2.center = (rn( 320, SCREEN_WIDTH - 150), 0)
-        #         self.image2 = pygame.image.load(os.path.join(f'C:/Pygame/cars/car_{j}.png'))
-        #         self.image2 = pygame.transform.scale(self.image2,(60,116))
-        
 
     def draw(self):
-        screen.blit(self.image1,self.rect1)
-        # screen.blit(self.image2,self.rect2)
-       
-
+        screen.blit(self.image1,self.rect)
         
 
-
+        
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
+        super().__init__()
+        self.colision = False
         self.image = pygame.image.load('C:/pp2_21B030767/Lab9/Racer/car_light_blue.png')
         self.image = pygame.transform.scale(self.image,(60,116))
         self.rect = self.image.get_rect()
@@ -111,27 +109,65 @@ class Player(pygame.sprite.Sprite):
     def draw(self):
         screen.blit(self.image,self.rect)
 
-    def collosion(self):
-        screen.fill((255,0,0))
-        font = pygame.font.Font(None,50)
-        game_over = font.render('Game Over', True , )
-        screen.blit(game_over,(300,100))
+cnt = 1
 
-E1 = Enemy()
-P1 = Player()
+class Coins(pygame.sprite.Sprite):
+    def __init__(self,im,speed):
+        super().__init__()
+        self.speed = speed
+     
+        self.image = pygame.image.load(f"C:/Pygame/Coins/coin_{im}.png")
+        self.image = pygame.transform.scale(self.image,(30,30))
+        self.rect = self.image.get_rect()
 
-# all_cars = pygame.sprite.Group()
-# all_cars.add(E1)
-# all_cars.add(P1)
-
-# enemies = pygame.sprite.Group()
-# enemies.add(E1)
- 
+        self.rect.center = (random.randint(80, SCREEN_WIDTH - 80), random.randint(350,400)) 
+        self.take = False
+   
+    def move(self,chainge_pos):
         
+        global cnt
+        self.rect.move_ip(0, self.speed)
+        if  self.take == True:
+            self.take = False
+            cnt += 1
+            self.rect.center = (random.randint( 120, 400), 0)
+        if self.take == 5:
+            cnt += 5
+            self.take = False
+            self.rect.center = (random.randint( 120, 400), 0)
+        if (self.rect.bottom > 600):
+            self.rect.top = 0
+            self.rect.center = (chainge_pos)
+    def draw(self):
+        screen.blit(self.image, self.rect)
 
 
+    
+E1 = Enemy(130,7,1)
+E2 = Enemy(350,9,2)
+P1 = Player()
+C1 = Coins(1 , 2)
+C2 = Coins(1, 1)
+C3 = Coins(2, 1)
 
+all_cars = pygame.sprite.Group()
+all_cars.add(E1)
+all_cars.add(P1)
+all_cars.add(E2)
 
+Coins = 0
+coin1 = pygame.sprite.Group()
+coin1.add(C1)
+coin2 = pygame.sprite.Group()
+coin2.add(C2)
+coin3 = pygame.sprite.Group()
+coin3.add(C3)
+
+enemies = pygame.sprite.Group()
+enemies.add(E1)
+enemies.add(E2)
+ 
+    
 
 while True:
     for event in pygame.event.get():
@@ -139,14 +175,54 @@ while True:
             pygame.quit()
             sys.exit()
    
-    # if pygame.sprite.spritecollideany(P1,):
-    #     P1.collosion()
-        
+    if pygame.sprite.spritecollideany(P1,enemies):
+        pygame.mixer.music.load('C:/Pygame/Sountracks/crash.wav')
+        pygame.mixer.music.play()
+        screen.blit(game_over, (0,0))
+        pygame.display.update()
+        for entity in all_cars:
+            entity.kill() 
+        time.sleep(5)
+        pygame.quit()
+        sys.exit() 
+
+
+    if pygame.sprite.spritecollideany(P1,coin1):
+       C1.take = True
+    
+    if pygame.sprite.spritecollideany(P1,coin2):
+       C2.take = True
+       
+    
+    if pygame.sprite.spritecollideany(P1,coin3):
+       C3.take = 5
+       
+    
+    if cnt%10 == 0:
+        E1.take = True
+        E2.take = True
+
     screen.blit(image_backround,(0,0))
+    screen.blit(surface_coin,(520,0))
+    
+    font = pygame.font.Font(None,30)
+    text = font.render(f'{cnt-1}', True, BLACK)
+    screen.blit(text,[550,0])
+
+    screen.blit(C1.image,(520,0))
     P1.draw()
     P1.move()
     E1.draw()
-    E1.move()
+    E1.move((rn(150,240),0))
+    E2.draw()
+    E2.move((rn(300,480),0))
+    C1.draw()
+    C1.move((rn(150,240),0))
+    C2.draw()
+    C2.move((rn(300,400),0))
+    C3.draw()
+    C3.move((rn(150,480),0))
+
 
     pygame.display.update()
 
